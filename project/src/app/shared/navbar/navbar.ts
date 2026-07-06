1baa0cc2fa438a02f53d8a5c99338dd523d17398
Sanidhya Sharma <Sanidhya.Sharma@euromonitor.com>
2026-07-06 10:28:51 +0530

    new

--- project/src/app/shared/navbar/navbar.ts
+++ project/src/app/shared/navbar/navbar.ts
@@ -3,12 +3,13 @@ import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
